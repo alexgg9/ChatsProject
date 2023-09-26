1fb700cd26f2edece44c1f4749cce57d9b61cc1b
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-09-26 09:06:43 +0200

    Clases del Modelo

--- src/main/java/AccesoADatos/model/domain/User.java
+++ src/main/java/AccesoADatos/model/domain/User.java
@@ -1,4 +1,36 @@
 package AccesoADatos.model.domain;
 
+import java.util.Objects;
+
 public class User {
+    private String nickname;
+
+    public User() {
+    }
+
+    public User(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(nickname, user.nickname);
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nickname='" + nickname + '\'' +
+                '}';
+    }
 }
